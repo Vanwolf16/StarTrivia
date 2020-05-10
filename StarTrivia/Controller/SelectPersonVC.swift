fc82d83968782edc88586ba0d800950ac663946f
David Murillo <d.murillo28@yahoo.com>
2020-05-10 15:50:57 -0700

    StarWars API

---
+++ StarTrivia/Controller/SelectPersonVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by David Murillo on 5/10/20.
+//  Copyright © 2020 MuriTech. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
