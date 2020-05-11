b2ad5bc32e68ea3d8b873cbc5d52840701cc6281
David Murillo <d.murillo28@yahoo.com>
2020-05-11 13:40:05 -0700

    Added some Pods

--- StarTrivia/Services/PersonApi.swift
+++ StarTrivia/Services/PersonApi.swift
@@ -8,8 +8,42 @@
 
 import Foundation
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PersonApi{
+    
+    //Web Request SwiftyJSON and AlamoFire
+    func getRandomPersonAlamo(id:Int,completion:@escaping PersonResponseCompletion){
+       
+        guard let url = URL(string: "\(PERSON_URL)\(id)") else {return}
+        AF.request(url).responseJSON { (response) in
+            if let error = response.error{
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+            guard let data = response.data else {return completion(nil)}
+            
+            do{
+                let json = try JSON(data: data)
+                let person = self.parsePersonSwifty(json: json)
+                completion(person)
+            }catch{
+                debugPrint(error.localizedDescription)
+                completion(nil)
+            }
+            
+          
+            
+        }
+        
+    
+    }//end of this Alamo
+    
+
+    //Web Request with URL Session
     func getRandomPersonUrlSession(id:Int,completion:@escaping PersonResponseCompletion){
        
         guard let url = URL(string: "\(PERSON_URL)\(id)") else {return}
@@ -17,6 +51,7 @@ class PersonApi{
             //check if error is nil
             guard error == nil else{
                 debugPrint(error.debugDescription)
+                completion(nil)
                 return
             }
             
@@ -54,5 +89,46 @@ class PersonApi{
         
         return person
     }
+    
+    private func parsePersonSwifty(json:JSON) -> Person{
+        let name = json["name"].stringValue
+        let height = json["height"].stringValue
+        let mass = json["mass"].stringValue
+        let hair = json["hair_color"].stringValue
+        let birthYear = json["birth_year"].stringValue
+        let gender = json["gender"].stringValue
+        let homeworldUrl = json["homeworld"].stringValue
+        let filmUrls = json["films"].arrayValue.map({$0.stringValue})
+        let vehicleUrls = json["vehicles"].arrayValue.map({$0.stringValue})
+        let starshipUrls = json["starships"].arrayValue.map({$0.stringValue})
+        
+        let person = Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeWorldUrl: homeworldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
+        
+        return person
+    }
+    
+    
+    /*
+     func getRandomPersonAlamo(id:Int,completion:@escaping PersonResponseCompletion){
+        
+         guard let url = URL(string: "\(PERSON_URL)\(id)") else {return}
+         AF.request(url).responseJSON { (response) in
+             if let error = response.error{
+                 debugPrint(error.localizedDescription)
+                 completion(nil)
+                 return
+             }
+             
+             guard let json = response.value as? [String:Any] else {return completion(nil)}
+             
+             let person = self.parsePersonManual(json: json)
+             completion(person)
+             
+         }
+         
+     
+     }//end of this Alamo
+     
+     */
     
 }
